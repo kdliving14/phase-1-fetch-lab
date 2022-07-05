5390d9fc7ac58111f8d07dfdc89b070c102d1c62
Karter <kdlmuzixnerd14@gmail.com>
2022-07-05 09:59:07 -0700

    commit tree e1f5735ec044d612d79c495334b26c328dc89730

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-function fetchBooks() {
+function fetchBooks() 
+{
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then(resp => resp.json())
+  .then(b => renderBooks(b))
+
 }
 
-function renderBooks(books) {
+function renderBooks(books) 
+{
   const main = document.querySelector('main');
-  books.forEach(book => {
+  books.forEach(book => 
+  {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
+document.addEventListener('DOMContentLoaded', function() {fetchBooks();});
